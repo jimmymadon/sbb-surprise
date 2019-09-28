c46ff35124a31763c4dfd9b8deb7078b9cdc819d
Anastasia Gracheva <anastasia.gracheva@jetbrains.com>
2019-09-28 23:31:36 +0200

    move data processing to app.py

--- service/main.py
+++ service/main.py
@@ -2,53 +2,10 @@ import requests
 import argparse
 
 from db_requests import select_random_cities, get_id_by_location
-from search_results import TripInfo, add_to_time
-#from app import call_for_offer_details, select_results
+from search_results import TripInfo, add_to_time, RequestParams, call_for_offer_details, select_results
 
 CLIENT_ID = "22ebc2be"
 CLIENT_SECRET = "2c820784f3e28837959abc43120989ca"
-CONTRACT_ID = "HAC222P"
-CONVERSATION_ID = "cafebabe-0815-4711-1234-ffffdeadbeef"  # make changable
-API_LINK = "https://b2p-int.api.sbb.ch/api/"
-PASSENGER_DEFAULT_INFO = "paxa;42;half-fare"
-
-
-def call_for_offer_details(trip_info, access_token):
-    headers = {"Accept-Language": "en",
-               "X-Contract-Id": CONTRACT_ID,
-               "X-Conversation-Id": CONVERSATION_ID,
-               "Authorization": ("Bearer " + access_token)
-               }
-    trip_id = trip_info["tripId"]
-    result = requests.get(f"{API_LINK}v2/prices?tripIds={trip_id}&passengers={PASSENGER_DEFAULT_INFO}", headers=headers)
-    if result.status_code != 200:
-        return None
-    return result.json()[0]
-
-
-def select_results(params, access_token):
-    headers = {"Accept-Language": "en",
-               "X-Contract-Id": CONTRACT_ID,
-               "X-Conversation-Id": CONVERSATION_ID,
-               "Authorization": ("Bearer " + access_token)
-               }
-
-    request_results = [requests.get(
-        f"{API_LINK}trips?arrivalDeparture=ED&date={params.departure_date}&" +
-        f"destinationId={destinationId}&originId={params.originId}&time={params.time}&trainType={params.train_type}",
-        headers=headers) for destinationId in params.destinationIds]
-
-    #print([r for r in request_results if r.status_code < 400])
-    print([r.json()[0]["segments"][0]["destination"]["name"] for r in request_results if r.status_code == 200])
-
-    request_results = [r.json() for r in request_results if r.status_code == 200]
-    offers_details = [[call_for_offer_details(offer, access_token) for offer in res] for res in request_results]
-    offers = dict([(destinationId, []) for destinationId in params.destinationIds])
-    for destinationId, destination_offers, destination_details in zip(params.destinationIds, request_results, offers_details):
-        for offer, details in zip(destination_offers, destination_details):
-            if details is not None:
-                offers[destinationId].append(TripInfo(offer, details))
-    return offers
 
 
 class RequestParams:
@@ -106,9 +63,11 @@ def main():
     #     print(offer.origin_stop.name, offer.origin_stop.time, offer.destination_stop.name, offer.destination_stop.time)
     #     print(o.origin_stop.name, o.origin_stop.time, o.destination_stop.name, o.destination_stop.time)
     #     print('=================')
-    best_ss = sorted(return_tickets)[0]
-    print(best_ss)
-    print(all_ss_tickets[best_ss])
+    if return_tickets:
+        best_ss = sorted(return_tickets)[0]
+        print(best_ss)
+    else:
+        print('No suggestions')
 
 
 
--- service/search_results.py
+++ service/search_results.py
@@ -1,4 +1,13 @@
 import datetime
+import requests
+
+
+CONTRACT_ID = "HAC222P"
+CONVERSATION_ID = "cafebabe-0815-4711-1234-ffffdeadbeef"  # make changable
+API_LINK = "https://b2p-int.api.sbb.ch/api/"
+PASSENGER_DEFAULT_INFO = "paxa;42;half-fare"
+DEFAULT_TRAIN_TYPE = "IR%3BICE%2FTGV%2FRJ%2CEC%2FIC"
+
 
 class Stop:
     def __init__(self, json_response):
@@ -20,3 +29,50 @@ def add_to_time(time, mins):
     ans = datetime.datetime.strptime(time, "%H:%M") + datetime.timedelta(minutes=30)
     return ans.strftime("%H:%M")
 
+
+class RequestParams:
+    def __init__(self, date, destinationIds, originId, time, train_type=DEFAULT_TRAIN_TYPE):
+        self.departure_date = date
+        self.destinationIds = destinationIds
+        self.originId = originId
+        h, min = time.split(':')
+        self.time = h + "%3A" + min
+        self.train_type = train_type
+
+
+def call_for_offer_details(trip_info, access_token):
+    headers = {"Accept-Language": "en",
+               "X-Contract-Id": CONTRACT_ID,
+               "X-Conversation-Id": CONVERSATION_ID,
+               "Authorization": ("Bearer " + access_token)
+               }
+    trip_id = trip_info["tripId"]
+    result = requests.get(f"{API_LINK}v2/prices?tripIds={trip_id}&passengers={PASSENGER_DEFAULT_INFO}", headers=headers)
+    if result.status_code != 200:
+        return None
+    return result.json()[0]
+
+
+def select_results(params, access_token):
+    headers = {"Accept-Language": "en",
+               "X-Contract-Id": CONTRACT_ID,
+               "X-Conversation-Id": CONVERSATION_ID,
+               "Authorization": ("Bearer " + access_token)
+               }
+
+    request_results = [requests.get(
+        f"{API_LINK}trips?arrivalDeparture=ED&date={params.departure_date}&" +
+        f"destinationId={destinationId}&originId={params.originId}&time={params.time}&trainType={params.train_type}",
+        headers=headers) for destinationId in params.destinationIds]
+
+    #print([r for r in request_results if r.status_code < 400])
+    print([r.json()[0]["segments"][0]["destination"]["name"] for r in request_results if r.status_code == 200])
+
+    request_results = [r.json() for r in request_results if r.status_code == 200]
+    offers_details = [[call_for_offer_details(offer, access_token) for offer in res] for res in request_results]
+    offers = dict([(destinationId, []) for destinationId in params.destinationIds])
+    for destinationId, destination_offers, destination_details in zip(params.destinationIds, request_results, offers_details):
+        for offer, details in zip(destination_offers, destination_details):
+            if details is not None:
+                offers[destinationId].append(TripInfo(offer, details))
+    return offers
