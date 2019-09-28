45b3c9e571fd21425772fa536f7b5e285d7cfd68
Luca Dolfi <ldolfi@gmail.com>
2019-09-28 02:57:11 +0200

    cleanup python backend

--- service/app.py
+++ service/app.py
@@ -1,31 +1,31 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
 
 flask_app = Flask(__name__)
 app = Api(app = flask_app, 
 		  version = "1.0", 
-		  title = "ML React App", 
-		  description = "Predict results using a trained model")
+		  title = "SBB Surprise App", 
+		  description = "WIP")
 
 name_space = app.namespace('prediction', description='Prediction APIs')
 
-model = app.model('Prediction params', 
-				  {'textField1': fields.String(required = True, 
-				  							   description="Text Field 1", 
-    					  				 	   help="Text Field 1 cannot be blank"),
-				  'textField2': fields.String(required = True, 
-				  							   description="Text Field 2", 
-    					  				 	   help="Text Field 2 cannot be blank"),
-				  'select1': fields.Integer(required = True, 
-				  							description="Select 1", 
-    					  				 	help="Select 1 cannot be blank"),
-				  'select2': fields.Integer(required = True, 
-				  							description="Select 2", 
-    					  				 	help="Select 2 cannot be blank"),
-				  'select3': fields.Integer(required = True, 
-				  							description="Select 3", 
-    					  				 	help="Select 3 cannot be blank")})
+# model = app.model('Prediction params', 
+# 				  {'textField1': fields.String(required = True, 
+# 				  							   description="Text Field 1", 
+#     					  				 	   help="Text Field 1 cannot be blank"),
+# 				  'textField2': fields.String(required = True, 
+# 				  							   description="Text Field 2", 
+#     					  				 	   help="Text Field 2 cannot be blank"),
+# 				  'select1': fields.Integer(required = True, 
+# 				  							description="Select 1", 
+#     					  				 	help="Select 1 cannot be blank"),
+# 				  'select2': fields.Integer(required = True, 
+# 				  							description="Select 2", 
+#     					  				 	help="Select 2 cannot be blank"),
+# 				  'select3': fields.Integer(required = True, 
+# 				  							description="Select 3", 
+#     					  				 	help="Select 3 cannot be blank")})
 
 # classifier = joblib.load('classifier.joblib')
 
@@ -39,22 +39,22 @@ class MainClass(Resource):
 		response.headers.add('Access-Control-Allow-Methods', "*")
 		return response
 
-	@app.expect(model)		
+	# @app.expect(model)		
 	def post(self):
 		try: 
-			formData = request.json
-			data = [val for val in formData.values()]
+			# formData = request.json
+			# data = [val for val in formData.values()]
 			# prediction = classifier.predict(data)
 			response = jsonify({
 				"statusCode": 200,
 				"status": "Prediction made",
-				"result": "Prediction: " + str(data)
+				"result": "Hello World!"
 				})
 			response.headers.add('Access-Control-Allow-Origin', '*')
 			return response
 		except Exception as error:
 			return jsonify({
 				"statusCode": 500,
-				"status": "Could not make prediction",
+				"status": "Error",
 				"error": str(error)
 			})
