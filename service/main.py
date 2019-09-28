75ec9326025f22f3e3f14ecda9f0c15d3f8d5898
Anastasia Gracheva <anastasia.gracheva@jetbrains.com>
2019-09-28 20:11:05 +0200

    fixes

--- service/main.py
+++ service/main.py
@@ -2,13 +2,15 @@ import requests
 import argparse
 
 from db_requests import select_random_cities, get_id_by_location
-from search_results import TripInfo
+from search_results import TripInfo, add_to_time
 
 
 CLIENT_ID = "22ebc2be"
 CLIENT_SECRET = "2c820784f3e28837959abc43120989ca"
 CONTRACT_ID = "HAC222P"
-CONVERSATION_ID = "cafebabe-0815-4711-1234-ffffdeadbeef"  # make changeble
+CONVERSATION_ID = "cafebabe-0815-4711-1234-ffffdeadbeef"  # make changable
+API_LINK = "https://b2p-int.api.sbb.ch/api/"
+PASSENGER_DEFAULT_INFO = "paxa;42;half-fare"
 
 
 def call_for_offer_details(trip_info, access_token):
@@ -17,7 +19,11 @@ def call_for_offer_details(trip_info, access_token):
                "X-Conversation-Id": CONVERSATION_ID,
                "Authorization": ("Bearer " + access_token)
                }
-    
+    trip_id = trip_info["tripId"]
+    result = requests.get(f"{API_LINK}v2/prices?tripIds={trip_id}&passengers={PASSENGER_DEFAULT_INFO}", headers=headers)
+    if result.status_code != 200:
+        return None
+    return result.json()[0]
 
 
 def select_results(params, access_token):
@@ -27,26 +33,29 @@ def select_results(params, access_token):
                "Authorization": ("Bearer " + access_token)
                }
 
-    request_results = [[requests.get(
-        f"https://b2p-int.api.sbb.ch/api/trips?arrivalDeparture=ED&date={params.departure_date}&" +
-        f"destinationId={destinationId}&originId={originId}&time={params.time}&trainType={params.train_type}",
-        headers=headers)
-        for destinationId in params.destinationIds] for originId in params.originIds]
-    request_results = [i for j in request_results for i in j]
+    request_results = [requests.get(
+        f"{API_LINK}trips?arrivalDeparture=ED&date={params.departure_date}&" +
+        f"destinationId={destinationId}&originId={params.originId}&time={params.time}&trainType={params.train_type}",
+        headers=headers) for destinationId in params.destinationIds]
 
     print([r for r in request_results if r.status_code < 400])
     print([r.json()[0]["segments"][0]["destination"]["name"] for r in request_results if r.status_code == 200])
 
-    offers_details = [call_for_offer_details(res, access_token) for res in request_results]
-    offers = [TripInfo(res, details) for res, details in zip(request_results, offers_details)]
+    request_results = [r.json() for r in request_results if r.status_code == 200]
+    offers_details = [[call_for_offer_details(offer, access_token) for offer in res] for res in request_results]
+    offers = dict([(destinationId, []) for destinationId in params.destinationIds])
+    for destinationId, destination_offers, destination_details in zip(params.destinationIds, request_results, offers_details):
+        for offer, details in zip(destination_offers, destination_details):
+            if details is not None:
+                offers[destinationId].append(TripInfo(offer, details))
     return offers
 
 
 class RequestParams:
-    def __init__(self, date, destinationIds, originIds, time, train_type):
+    def __init__(self, date, destinationIds, originId, time, train_type):
         self.departure_date = date
         self.destinationIds = destinationIds
-        self.originIds = originIds
+        self.originId = originId
         h, min = time.split(':')
         self.time = h + "%3A" + min
         self.train_type = train_type
@@ -63,19 +72,26 @@ def main():
 
     start_location = args.starting_location
     departure_date = args.date
-    destinationIds = select_random_cities(5)
-    originIds = get_id_by_location(start_location)
-    search_params = RequestParams(departure_date, destinationIds, originIds, args.time, args.train_type)
+    destinationIds = select_random_cities(10)
+    originId = get_id_by_location(start_location)
+    destinationIds = [8503000, 8505000]
+    originId = 8502204
+    search_params = RequestParams(departure_date, destinationIds, originId, args.time, args.train_type)
 
     auth = requests.post("https://sso-int.sbb.ch/auth/realms/SBB_Public/protocol/openid-connect/token",
                         data={"grant_type": "client_credentials",
                               "client_id": CLIENT_ID, "client_secret": CLIENT_SECRET})
     access_token = auth.json()["access_token"]
 
-    offers_list = select_results(destinationIds, access_token)
-    return_offers_list = select_results(destinationIds, access_token)
-    for offer in offers_list:
-        print(offer.origin_stop)
+    offers_dict = select_results(search_params, access_token)
+
+    for destinationId, destination_offers in offers_dict.values():
+        for offer in destination_offers:
+            search_params = RequestParams(departure_date, [originId], destinationId,
+                                          add_to_time(offer.destination_stop.time, 30), args.train_type)
+            back_offers_dict = select_results(search_params, access_token)
+            print(back_offers_dict, originId)
+            print(add_to_time(offer.destination_stop.time, 30))
 
 
 
--- service/search_results.py
+++ service/search_results.py
@@ -1,12 +1,22 @@
+import datetime
+
 class Stop:
-    def __init__(self, name, date, time):
-        self.name = name
-        self.date = date
-        self.time = time
+    def __init__(self, json_response):
+        self.name = json_response["name"]
+        self.date = json_response["date"]
+        self.time = json_response["time"]
 
 
 class TripInfo:
-    def __init__(self, json_response):
+    def __init__(self, json_response, json_details):
         self.origin_stop = Stop(json_response["segments"][0]["origin"])
         self.destination_stop = Stop(json_response["segments"][0]["destination"])
-        self.tripId = json_response["trip_id"]
+        self.tripId = json_response["tripId"]
+        self.price = int(json_details["price"])
+        self.superSaver = json_details["superSaver"]
+
+
+def add_to_time(time, mins):
+    ans = datetime.datetime.strptime(time, "%H:%M") + datetime.timedelta(minutes=30)
+    return ans.strftime("%H:%M")
+
