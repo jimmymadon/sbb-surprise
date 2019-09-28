d39f6f4e4c4bc80d83e576927aac7ef010c17eab
Sofya Ozherelieva <sofya.ozherelieva@yandex.ru>
2019-09-29 01:59:53 +0200

    db_req

--- service/db_requests.py
+++ service/db_requests.py
@@ -45,14 +45,42 @@ def get_picture_by_id(id_):
             return df.iloc[index].images
 
 
-def select_random_cities(quantity=5):
+class Event:
+    def __init__(self, stop_id, stop_name, event_name, description, categories, url):
+        self.stop_id = stop_id
+        self.stop_name = stop_name
+        self.event_name = event_name
+        self.description = description
+        self.categories = categories
+        self.url = url
+
+
+def get_event_list_by_id(id_):
+    df = pd.read_csv("../events_description_df.csv", sep=',')
+    event_list = []
+    stop_name = list(df.stop_name)
+    event_name = list(df.event_name)
+    description = list(df.description)
+    categories = list(df.categories)
+    url = list(df.url)
+
+    for index, stop_id in enumerate(list(df.stop_id)):
+        if id_ == stop_id:
+            event_list.append(Event(stop_id, stop_name[index],
+                                    event_name[index], description[index],
+                                    categories[index], url[index]))
+    return event_list
+
+
+def select_random_cities(quantity=5, file_name="../train_stations_data.csv"):
     """
     :param quantity: number of cities
     :return: random ids of cities
     """
     result_id_list = []
-    df = pd.read_csv("../train_stations_data.csv", sep=',')
+    df = pd.read_csv(file_name, sep=',')
     for i in range(quantity):
         rand_number = randint(0, df.shape[0] - 1)
         result_id_list.append(df.iloc[rand_number].stop_id)
     return result_id_list
+
